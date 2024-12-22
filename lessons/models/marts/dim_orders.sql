11c7434139dc5a3e631db53dc20fa5c6639b53d1
Anton Kuzmin <anton.kuzmin.external@zalando.de>
2024-12-22 15:47:18 +0400

    added for loop functionality

--- lessons/models/marts/dim_orders.sql
+++ lessons/models/marts/dim_orders.sql
@@ -22,6 +22,11 @@ order_item_measures AS (
 		SUM(item_profit) AS total_profit,
 		SUM(item_discount) AS total_discount,
 
+		{%- for department_name in dbt_utils.get_column_values(table=ref('int_ecommerce__order_items_products'), column='product_department') %}
+			SUM(IF(product_department = '{{department_name}}', item_sale_price, 0)) AS total_sold_{{department_name.lower()}}swear{% if not loop.last %},{% endif -%}
+		{% endfor %}
+
+
 	FROM {{ ref('int_ecommerce__order_items_products') }}
 	GROUP BY 1
 )
@@ -39,6 +44,10 @@ SELECT
 	om.total_product_cost,
 	om.total_profit,
 	om.total_discount,
+
+	{%- for department_name in departments %}
+	om.total_sold_{{department_name.lower()}}swear,
+	{%- endfor %}
 
 	TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
 
